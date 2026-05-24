200a5de8e41a8ecd8de0dae6741d3c54c5b6b64b
Max Barrett <madmaxlgndklr@gmail.com>
2026-05-24 17:02:40 -0400

    fix: gradle.properties comment, database visibility, plan KSP version

--- app/src/main/java/com/madmaxlgndklr/pokedex/PokedexApplication.kt
+++ app/src/main/java/com/madmaxlgndklr/pokedex/PokedexApplication.kt
@@ -6,7 +6,7 @@ import com.madmaxlgndklr.pokedex.data.remote.RetrofitClient
 import com.madmaxlgndklr.pokedex.data.repository.PokemonRepository
 
 class PokedexApplication : Application() {
-    val database by lazy { AppDatabase.getInstance(this) }
+    private val database by lazy { AppDatabase.getInstance(this) }
     val repository by lazy {
         PokemonRepository(RetrofitClient.api, database.caughtPokemonDao())
     }
