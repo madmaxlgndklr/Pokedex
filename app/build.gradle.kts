9eec0b722b6a3c4faaa135e407d4fb9bbf9baa9b
Max Barrett <madmaxlgndklr@gmail.com>
2026-05-31 15:05:35 -0400

    chore: bump version to 0.32 (versionCode 32)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.madmaxlgndklr.pokedex"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 32
+        versionName = "0.32"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val localProps = org.jetbrains.kotlin.konan.properties.Properties()
         localProps.load(rootProject.file("local.properties").inputStream())
